6372e7997946038d6ac23e09c32f800eed90cae0
dujulien <du.julien1@gmail.com>
2019-11-27 01:03:28 +0100

    Production config set

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,5 +13,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "images")
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
 Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets")
 Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "javascripts")
