dad6d96b135659072419cbc8e876876b6e66cd09
Marius-creator <marius.martinez40@gmail.com>
2019-11-27 17:26:54 +0100

    bouton passer commande

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,10 +1,11 @@
 class CartsController < ApplicationController
-	
+before_action :authenticate_user!
 	def index
 	end
 
 	def show
-		@cart = Cart.find_by(user_id: 2)
+		@cart = Cart.find(params[:id])
+		@order = Order.find(params[:id])
 	end
 
 	def new
@@ -20,6 +21,10 @@ class CartsController < ApplicationController
 	end
 
 	def destroy
+    @order = current_cart
+    @order_item = @order.joint_table_order_items.find(params[:id])
+    @order_item.destroy
+    @order_items = @order.joint_table_order_items
 	end
 
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,34 @@
 class OrdersController < ApplicationController
-  def show
-		@order = Order.find_by(user_id: 2)
-	end
+
+    before_action :authenticate_user!
+
+  	def index
+  		@orders = Orders.all
+  	end
+
+  	def show
+  		@order = Order.find(params[:id])
+  		@ordered_items = JointTableOrderItem.where(order_id: @order.id)
+  		@purchase = []
+      	@ordered_items.each do |item|
+        		@purchase << Item.find(item.item_id)
+      	end
+  	end    
+
+  	def new
+  	end
+
+  	def create
+  		@order = Order.create
+  	end
+
+  	def edit
+  	end
+
+  	def update
+  	end
+
+  	def destroy
+  	end
+
 end
