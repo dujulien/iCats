bf9a3ff10bcc5177d335b93003b04ddb80b87b50
Yoni <y_benatar@hotmail.com>
2019-11-27 17:00:50 +0100

    mailer update

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,7 +10,7 @@ class UserMailer < ApplicationMailer
     @url  = 'http://monsite.fr/login' 
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-    mail(to: @user.email, subject: 'Bienvenue chez nous !') 
+    mail(to: @user.email, subject: 'Bienvenue sur iCats !') 
   end
 
 end
