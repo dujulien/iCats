6076528ad64c745eb694a7abc91689c4c24f31ad
dujulien <du.julien1@gmail.com>
2019-11-27 13:09:27 +0100

    Action Mailer done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,7 +62,6 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   #LETTER OPENER CONFIG
-  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
 
