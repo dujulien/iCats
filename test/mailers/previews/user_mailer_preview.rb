b1e30f0ce19c934d046beaa36703b8b340d63276
Yoni <y_benatar@hotmail.com>
2019-11-28 22:42:43 +0100

    Mailer update

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,14 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
-# Preview this email at http://localhost:3000/rails/mailers/user_mailer/welcome_email
-	def welcome_email
-		UserMailer.welcome_email(User.last)
-	end
 
-# Preview this email at http://localhost:3000/rails/mailers/user_mailer/order_email
-	def order_email
-		UserMailer.order_email(User.last)
-	end
 
 end
